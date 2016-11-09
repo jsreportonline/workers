56db0fddfae489cafa07ea58f7a0823999a9e645
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-11-09 17:52:05 +0100

    log failed requests

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const mongo = require('./lib/mongo')
 const ping = require('./lib/ping')
 const status = require('./lib/status')
 const execute = require('./lib/execute')
+const winston = require('winston')
+const path = require('path')
+const fs = require('fs')
+
+try {
+  fs.mkdirSync(path.join(__dirname, 'logs'))
+} catch (e) {
+
+}
+
+winston.remove(winston.transports.Console)
+winston.add(winston.transports.Console, {
+  level: 'info'
+})
+winston.add(winston.transports.File, {
+  filename: path.join(__dirname, 'logs', 'workers.log'),
+  json: false,
+  maxsize: 50000000,
+  maxFiles: 5,
+  timestamp: true,
+  level: 'debug'
+})
+
+winston.info('starting workers')
 
 const error = (err, res) => {
+  winston.error(err)
   res.statusCode = 500
   res.setHeader('Content-Type', 'text/plain')
   return res.end(err.stack)
@@ -46,7 +71,7 @@ mongo().then(() => {
 
   server.listen(1000)
 }).catch((e) => {
-  console.error(e)
+  winston.error(e)
   process.exit(1)
 })
 
